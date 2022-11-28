062c7c668b546028d5265388af48651d5796cbed
tracykagai-2022 <tracy.kagai@student.moringaschool.com>
2022-11-28 12:28:24 +0300

    Done

---
+++ index.js
@@ -0,0 +1,24 @@
+function Monday() {
+    console.log("Go for a five-mile run");
+    console.log("Pump iron");
+  }
+  
+  function Tuesday() {
+    console.log("Go for a five-mile run");
+    console.log("Swim 40 laps");
+  }
+  
+  function Wednesday() {
+    console.log("Go for a five-mile run");
+    console.log("Go for a five-mile run");
+  }
+  
+  function Thursday() {
+    console.log("Go for a five-mile run");
+    console.log("Pump iron");
+  }
+  
+  function Friday() {
+    console.log("Go for a five-mile run");
+    console.log("Swim 40 laps");
+  }
